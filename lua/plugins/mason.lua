3e761d924d3689c1519856b21f382def3f98b89f
Ryaguzov Michael <miha890r@gmail.com>
2025-07-11 00:24:02 +0500

    Add correct usage of csharpier

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,8 +20,20 @@ return {
   {
     "seblyng/roslyn.nvim",
     ft = "cs",
-    --@module 'roslyn.config'
-    --@type RoslynNvimConfig
     opts = {},
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+      },
+      formatters = {
+        csharpier = {
+          command = "home/famine/.dotnet/tools/csharpier",
+          args = { "--write-stdout", "--single-file" },
+        },
+      },
+    },
   },
 }
